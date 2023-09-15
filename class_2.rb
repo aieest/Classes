d82bb90dc713e52ea9945b922e00ef0dcd1f3314
August Estidola <aie.estidola@gmail.com>
2023-09-15 15:12:08 +0800

    Class 2: Car Rental

---
+++ class_2.rb
@@ -0,0 +1,18 @@
+class Car_renter
+  attr_accessor :name, :phone, :email, :days_rent, :car_plate
+
+  def initialize()
+    @name = name
+    @phone = phone
+    @email = email
+    @car_rented = car_rented
+    @car_plate = car_plate
+  end
+
+  def
+  end
+
+    class Rental_System
+
+    end
+    # Test code
